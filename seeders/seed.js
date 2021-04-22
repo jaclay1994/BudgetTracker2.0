17abdc0092c459a90e411d16da04899f72bcc172
jaclay1994 <jaclay94@gmail.com>
2021-04-21 22:13:19 -0400

    Updated seeds file.

--- seeders/seed.js
+++ seeders/seed.js
@@ -4,3 +4,28 @@ var db = require("../models")
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
     useNewUrlParser: true
 });
+
+var budgetSeed = [
+    {
+        name: " Cooler",
+        value: 25,
+        date: new Date(Date.now())
+    },
+    {
+        name: "CD",
+        value: 90,
+        date: new Date(Date.now())
+    },
+    {
+        name: "Earphones",
+        value: 56,
+        date: new Date(Date.now())
+    },
+    {
+        name: "Telephone",
+        value: 70,
+        date: new Date(Date.now())
+    }
+]
+
+db.Transaction.delete({})
